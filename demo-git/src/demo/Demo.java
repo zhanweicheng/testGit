6aedde3c36e1eccb69b674d836c85b792d4c963b
zhanwei <15827576253@163.com>
2019-12-12 23:15:50 +0800

    test

--- demo-git/src/demo/Demo.java
+++ demo-git/src/demo/Demo.java
@@ -4,5 +4,6 @@ public class Demo {
 	public static void main(String[] args) {
 		System.out.println("HelloWorld!");
 		System.out.println("HelloWorld!");
+		System.out.println("HelloWorld!");
 	}
 }
